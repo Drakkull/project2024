c4d3372c9c5f4607cab5890399b14c8c65b92948
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-02-16 15:01:40 -0300

    1st Commit

---
+++ src/main/java/amicosCode/test2024/student/Student.java
@@ -0,0 +1,14 @@
+package amicosCode.test2024.student;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+
+}

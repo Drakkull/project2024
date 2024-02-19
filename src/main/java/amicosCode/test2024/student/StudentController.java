24733a5508766843aa486ac6e3419165e0c98be0
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-02-19 15:36:58 -0300

    Created connection with DB (PostGres)

--- src/main/java/amicosCode/test2024/student/StudentController.java
+++ src/main/java/amicosCode/test2024/student/StudentController.java
@@ -1,6 +1,7 @@
 package amicosCode.test2024.student;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class StudentController {
     private final StudentService studentService;
 
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/amicosCode/test2024/student/StudentService.java
+++ src/main/java/amicosCode/test2024/student/StudentService.java
@@ -1,7 +1,10 @@
 package amicosCode.test2024.student;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents(){
         return List.of( );

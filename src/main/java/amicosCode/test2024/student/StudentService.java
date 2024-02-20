79fe03b6d6b5f245c213301f4e34182ff6418464
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-02-20 10:16:58 -0300

    Added StudentRepository and implemented Repository inside the StudentService

--- src/main/java/amicosCode/test2024/student/StudentService.java
+++ src/main/java/amicosCode/test2024/student/StudentService.java
@@ -1,13 +1,25 @@
 package amicosCode.test2024.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
     public List<Student> getStudents(){
-        return List.of( );
+
+        return studentRepository.findAll();
     }
 
 }

cffd5bed54af54cd5fb73339d400e3636b5ed894
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-02-20 14:43:23 -0300

    Added Transient anotation into age and method was reworked to give age using LocalDate

--- src/main/java/amicosCode/test2024/student/Student.java
+++ src/main/java/amicosCode/test2024/student/Student.java
@@ -3,6 +3,7 @@ package amicosCode.test2024.student;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table
@@ -16,6 +17,7 @@ public class Student {
     private String name;
     private String email;
     private LocalDate dob;
+    @Transient
     private Integer age;
 
 
@@ -23,19 +25,19 @@ public class Student {
 
     }
 
-    public Student(long id, String name, String email, LocalDate dob, Integer age) {
+    public Student(long id, String name, String email, LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
+
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
+    public Student(String name, String email, LocalDate dob) {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
+
     }
 
     public long getId() {
@@ -72,7 +74,7 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob,LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- src/main/java/amicosCode/test2024/student/StudentConfiguration.java
+++ src/main/java/amicosCode/test2024/student/StudentConfiguration.java
@@ -16,9 +16,9 @@ public class StudentConfiguration {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-            Student judith= new Student("Judith","judith.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7), 27);
+            Student judith= new Student("Judith","judith.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7));
 
-            Student chulapa= new Student("Chulapa","chulapa.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7), 27);
+            Student chulapa= new Student("Chulapa","chulapa.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7));
 
 
         repository.saveAll(List.of(judith,chulapa));

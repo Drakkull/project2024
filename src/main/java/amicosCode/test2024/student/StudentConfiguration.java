0208c0a43a1380c8d16d1f0f1d291a44af6431a7
Leonardo DiAbreu <drakkull007@hotmail.com>
2024-02-20 10:49:53 -0300

    Added StudentConfiguration and 2 instances os students as example

---
+++ src/main/java/amicosCode/test2024/student/StudentConfiguration.java
@@ -0,0 +1,30 @@
+package amicosCode.test2024.student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfiguration {
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student judith= new Student("Judith","judith.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7), 27);
+
+            Student chulapa= new Student("Chulapa","chulapa.biriba@gmail.com",LocalDate.of(1995, Month.JANUARY,7), 27);
+
+
+        repository.saveAll(List.of(judith,chulapa));
+        };
+
+
+    }
+
+}
